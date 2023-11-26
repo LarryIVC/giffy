e019a58812c66987a81eeadda0eb80100bc7ddc2
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-11-26 19:26:30 -0400

    video 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const INITIAL_VALUE = [
+  'https://media2.giphy.com/media/TObbUke0z8Mo/200.webp?cid=ecf05e4723w46d8cz68tpsh60rjxvch4wdvch3xxqglfosug&ep=v1_gifs_search&rid=200.webp&ct=g',
+  'https://media2.giphy.com/media/nntN4XMNwTzZ6/200.webp?cid=ecf05e4723w46d8cz68tpsh60rjxvch4wdvch3xxqglfosug&ep=v1_gifs_search&rid=200.webp&ct=g'
+];
 function App() {
+  const [value, setValue] = useState([INITIAL_VALUE]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        {
+          value.map((gif, Index) => <img key={Index} src={gif} alt='Gif' />)
+        }
+
+      </section>
     </div>
   );
 }
