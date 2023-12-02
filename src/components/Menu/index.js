0ddb3a26cc852af627c05ec2a6aa2285b2f46f81
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-12-02 00:02:02 -0400

    Add Menu component with dynamic trends

---
+++ src/components/Menu/index.js
@@ -0,0 +1,33 @@
+import { Link } from 'wouter';
+import './Menu.css';
+import getTrends from 'services/getTrends'
+import { useEffect, useState } from 'react';
+
+const Menu = () => {
+  const [trends, setTrends] = useState([])
+
+  useEffect(() => {
+    getTrends().then(setTrends)
+    // const newTrends = trends.splice(0, 7)
+    // setTrends(newTrends)
+  }, [])
+
+  return (
+    <nav className='menu'>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        {
+          trends.map((trend) => {
+            return (
+              <li key={trend}>
+                <Link to={`/gif/${trend}`}>{trend[0].toUpperCase() + trend.slice(1)}</Link>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </nav>
+  )
+}
+
+export default Menu;
