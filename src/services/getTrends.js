0792b98c91097d3afbdd44b6d4b4eca326face62
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-12-02 00:00:56 -0400

    Add getTrends service function

---
+++ src/services/getTrends.js
@@ -0,0 +1,14 @@
+import { API_URL, API_KEY } from './constants';
+
+const getTrends = () => {
+  const URL = `${API_URL}/trending/searches?api_key=${API_KEY}`
+  return fetch(URL)
+    .then((res) => res.json())
+    .then((response) => {
+      const { data } = response;
+      console.log(data);
+      return data.splice(0, 7);
+    })
+}
+
+export default getTrends;
