14370d3d94a8f6256662ba85ec735e943af6b2c1
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-12-13 20:03:32 -0400

    Refactor getGifs function to include default values for limit, keyword, and page

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,7 +1,7 @@
 import { API_URL, API_KEY } from "./constants";
 
-const getGifs = ({ keyword }) => {
-  const URL = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=12&offset=0&rating=pg&lang=en&bundle=messaging_non_clips`
+const getGifs = ({ limit= 12,keyword='panda', page=0 } = {}) => {
+  const URL = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=${limit}&offset=${page*limit}&rating=pg&lang=en&bundle=messaging_non_clips`
   return fetch(URL)
     .then((res) => res.json())
     .then((response) => {
