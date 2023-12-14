3b4de203e94b5a6e7aa0a931f01c2f5e0a0d3a20
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-12-13 20:03:13 -0400

    Refactor useGifs hook to include pagination and local storage

--- src/Hooks/useGifs.js
+++ src/Hooks/useGifs.js
@@ -2,18 +2,44 @@ import { useEffect, useState, useContext } from "react";
 import getGifs from "services/getGifs";
 import GifContext from "context/GifContext";
 
+const INITIAL_PAGE = 0;
 export const useGifs = ({ keyword }) => {
   const { gifs, setGifs } = useContext(GifContext)
   const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(INITIAL_PAGE);
+  const [loadingPage, setLoadingPage] = useState(false);
+  
+  const currentKeyword = keyword || localStorage.getItem('lastKeyword') || 'panda';
+
+  const saveLocalStorage = (keyword) => {
+    if (localStorage.getItem('lastKeyword') !== keyword){
+      keyword && localStorage.setItem('lastKeyword', keyword);
+      setPage(INITIAL_PAGE);
+    }
+  }
 
   useEffect(() => {
     setLoading(true);
-    const currentKeyword = keyword || localStorage.getItem('lastKeyword') || 'panda';
+    if (localStorage.getItem('lastKeyword') === keyword) return
     getGifs({ keyword: currentKeyword }).then(gifs => {
       setGifs(gifs)
       setLoading(false);
-      localStorage.setItem('lastKeyword', keyword);
+      saveLocalStorage(keyword)
     });
-  }, [keyword, setGifs]);
-  return { loading, gifs }
+  }, [keyword, setGifs, currentKeyword]);
+
+  useEffect(() => {
+    if (page === INITIAL_PAGE) return
+
+    setLoadingPage(true)
+    getGifs({ keyword: currentKeyword, page })
+      .then(nextsGifs => {
+        setGifs(prevGifs => [...prevGifs, ...nextsGifs])
+        setLoadingPage(false)
+        saveLocalStorage(keyword)
+      })
+
+  }, [page, currentKeyword, setGifs, keyword])
+
+  return { loading, gifs, setPage, loadingPage }
 }
