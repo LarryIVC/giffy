58feb39820e1d68effb9169f755e4220178c3cde
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-12-21 10:44:45 -0400

    Add debounce function to constants.js

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,2 +1,12 @@
 export const API_URL = `https://api.giphy.com/v1`
 export const API_KEY = '3ZKkAdDmGkJ1XVHB0LnFewAfPx08s3y2'
+
+export function debounce(callback, wait) {
+  let timerId;
+  return (...args) => {
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      callback(...args);
+    }, wait);
+  };
+}
