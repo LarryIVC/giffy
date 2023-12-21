9c7a5733ff96267f6b2bb29d96604e8cafd21a0d
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-12-21 10:44:21 -0400

    Add lazy loading functionality

--- src/components/ListOfGifs/index.js
+++ src/components/ListOfGifs/index.js
@@ -2,18 +2,30 @@ import Gif from 'components/Gif'
 import Loader from 'components/Loader'
 import { useGifs } from 'Hooks/useGifs'
 import './ListOfGifs.css'
+import useNearScreen from 'Hooks/useNearScreen'
+import { useEffect, useRef } from 'react'
 
 const ListOfGifs = ({ params }) => {
   if (params === undefined || params === null) {
     params = { keyword: null }
   }
   const { keyword } = params;
-
+  const externalRef = useRef()
   const { loading, gifs, setPage } = useGifs({ keyword })
+  const { isNear } = useNearScreen({ externalRef: loading ? null : externalRef })
 
   const handleNextPage = () => {
+    console.log('Next Page')
     setPage(prevPage => prevPage + 1)
   }
+
+  // console.log(isNear)
+
+  // const handleNextPage = () => console.log('Next Page')
+
+  useEffect(() => {
+    if (isNear) handleNextPage()
+  },[isNear])
 
   return (
     <section className="App-content">
@@ -23,7 +35,8 @@ const ListOfGifs = ({ params }) => {
             return (<Gif key={id} title={title} url={url} id={id} />)
           })
       }
-      <button onClick={handleNextPage} className='next-page'>Next Page</button>
+      <div id='visor' ref={externalRef}></div>
+      {/* <button onClick={handleNextPage} className='next-page'>Next Page</button> */}
     </section>
   )
 }
