43b717e041740b66f35e315e1b5a2cbb85bb1818
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-12-30 01:34:39 -0400

    Add console logs to debug code

--- src/Pages/Details/index.js
+++ src/Pages/Details/index.js
@@ -4,8 +4,11 @@ import Gif from "components/Gif"
 import './Details.css'
 
 const Detail = ({ params }) => {
+  console.log(params.id)
   const { gifs } = useContext(GifsContext)
+  // console.log(gifs)
   const gif = gifs.find(gif => gif.id === params.id)
+  // console.log(gif)
 
   return (
     <section className="gif-detail-container">
