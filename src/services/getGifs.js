7ee2543f195dafeca526c85f2594e90d236d3a0b
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-12-30 01:34:48 -0400

    Update limit in getGifs function and add console.log statement

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,7 +1,8 @@
 import { API_URL, API_KEY } from "./constants";
 
-const getGifs = ({ limit = 6, keyword = 'panda', page = 0 } = {}) => {
+const getGifs = ({ limit = 12, keyword = 'panda', page = 0 } = {}) => {
   const URL = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=${limit}&offset=${page * limit}&rating=pg&lang=en&bundle=messaging_non_clips`
+  console.log(keyword)
   return fetch(URL)
     .then((res) => res.json())
     .then((response) => {
