0d24a694879989ca4cd81141a2f58fd4e0f3cdb9
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2024-01-05 18:09:22 -0400

    Add meta tags for SEO in Details page

--- src/Pages/Details/index.js
+++ src/Pages/Details/index.js
@@ -3,19 +3,26 @@ import './Details.css'
 import useSingleGif from "Hooks/useSingleGif"
 import Loader from "components/Loader"
 import { Redirect } from "wouter"
+import { Helmet } from "react-helmet"
 
 const Detail = ({ params }) => {
   console.log(params.id)
-  
+
   const { gif, isLoading, isError } = useSingleGif({ id: params.id })
   console.log(isError)
-  if(isLoading) return <Loader />
-  if(isError) return <Redirect to='/404' />
+  if (isLoading) return <Loader />
+  if (isError) return <Redirect to='/404' />
 
   return (
-    <section className="gif-detail-container">
-      <Gif {...gif} />
-    </section>
+    <>
+      <Helmet>
+        <title>Giffy | Details | {gif.title}</title>
+        <meta name="description" content={`Giffy app | details for gif file | ${gif.title}`} />
+      </Helmet>
+      <section className="gif-detail-container">
+        <Gif {...gif} />
+      </section>
+    </>
   )
 }
 
