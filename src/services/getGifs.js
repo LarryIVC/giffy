3819e05050aa2e48bfdbd33e3b3ca4ad86c39c78
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2024-01-07 23:23:22 -0400

    Update getGifs function to include rating parameter

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,7 +1,7 @@
 import { API_URL, API_KEY } from "./constants";
 
-const getGifs = ({ limit = 12, keyword = 'panda', page = 0 } = {}) => {
-  const URL = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=${limit}&offset=${page * limit}&rating=g&lang=en&bundle=messaging_non_clips`
+const getGifs = ({ limit = 12, keyword = 'panda', page = 0, rating='g' } = {}) => {
+  const URL = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=${limit}&offset=${page * limit}&rating=${rating}&lang=en&bundle=messaging_non_clips`
   // console.log(page, limit, URL)
   return fetch(URL)
     .then((res) => res.json())
